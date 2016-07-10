a44dab1b132205c5650af0cf8380b1f11a5b7795
RaulAvila <ravila80@gmail.com>
2016-07-10 18:13:54 +0100

    Antipatrones arquitectura

---
+++ src/main/java/com/raulavila/lesson04/numberofdiscintersections/Solution.java
@@ -0,0 +1,8 @@
+package com.raulavila.lesson04.numberofdiscintersections;
+
+class Solution {
+    public int solution(int[] A) {
+        //TODO
+        return 0;
+    }
+}
---
+++ src/main/java/com/raulavila/lesson5/stonewall/Solution.java
@@ -0,0 +1,8 @@
+package com.raulavila.lesson5.stonewall;
+
+class Solution {
+    public int solution(int[] H) {
+
+        return 0;
+    }
+}
